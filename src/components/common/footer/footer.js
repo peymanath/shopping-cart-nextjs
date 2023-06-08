7ccbc28747f1071ded19c115ddb15e4d3c08b9d8
peymanath <naderidefault@gmail.com>
2023-06-08 16:02:44 +0330

    change footer link

--- src/components/common/footer/footer.js
+++ src/components/common/footer/footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Link from "next/link";
 const Footer = props => {
     return (
         <div className="bg-primary text-white fixed bottom-0 w-full">
             <div className="container py-3 text-center">
-                Powered By <a href="" className="hover:text-white/60">Peyman Naderi</a>
+                Powered By <Link href="https://peymanath.ir" className="hover:text-white/60">Peyman Naderi</Link>
             </div>
         </div>
     )
